074a439a8b3907a30c30bd09b35b8d08da0bb54f
Leander Riefel <riefel.leander@gmail.com>
2025-10-13 00:29:19 +0200

    fix build? v2

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import { nitro } from "nitro/vite"
 import { fileURLToPath } from "node:url"
 import { resolve } from "node:path"
 
@@ -23,5 +24,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["cubing"],
   },
-  plugins: [tanstackStart(), react(), tailwindcss()],
+  plugins: [
+    tanstackStart(),
+    nitro({
+      config: {
+        preset: "vercel",
+      },
+    }),
+    react(),
+    tailwindcss(),
+  ],
 })
